2c107e6e6e1943bdeb63037bbcb60cf414b10a49
shunkosa <shun@fastmail.com>
2023-07-18 20:54:23 +0900

    ✨ Add message for server-side error

--- force-app/main/knowledge-category-list-view/lwc/knowledgeCategoryListView/knowledgeCategoryListView.js
+++ force-app/main/knowledge-category-list-view/lwc/knowledgeCategoryListView/knowledgeCategoryListView.js
@@ -9,9 +9,11 @@ import KCLV_CollapseAll from '@salesforce/label/c.KCLV_CollapseAll';
 import KCLV_Search from '@salesforce/label/c.KCLV_Search';
 import KCLV_MessageArticleFound from '@salesforce/label/c.KCLV_MessageArticleFound';
 import KCLV_MessageArticleNotFound from '@salesforce/label/c.KCLV_MessageArticleNotFound';
+import KCLV_ErrorHeader from '@salesforce/label/c.KCLV_ErrorHeader';
 
 export default class KnowledgeCategoryListView extends LightningElement {
     loading;
+    errorMessage;
 
     @track trees;
     columns = [];
@@ -27,17 +29,24 @@ export default class KnowledgeCategoryListView extends LightningElement {
         KCLV_CollapseAll,
         KCLV_Search,
         KCLV_MessageArticleFound,
-        KCLV_MessageArticleNotFound
+        KCLV_MessageArticleNotFound,
+        KCLV_ErrorHeader
     };
 
     async connectedCallback() {
         this.loading = true;
-        this.trees = await getCategoryTree();
-        const columns = await getFormattedColumns();
-        this.columns = columns.map((column, index) => {
-            return this.formatColumn(column, index);
-        });
-        this.loading = false;
+        try {
+            this.trees = await getCategoryTree();
+            const columns = await getFormattedColumns();
+            this.columns = columns.map((column, index) => {
+                return this.formatColumn(column, index);
+            });
+        } catch (e) {
+            console.error(e);
+            this.errorMessage = this.extractErrorMessage(e);
+        } finally {
+            this.loading = false;
+        }
     }
 
     /**
@@ -113,31 +122,30 @@ export default class KnowledgeCategoryListView extends LightningElement {
             return;
         }
         this.loading = true;
-        const articles = await getArticles({
-            categoryGroupName: this.selectedCategoryGroupName,
-            categoryName: this.selectedCategoryName
-        });
-        console.log(articles);
-        this.articles = articles.map((r) => ({
-            recordUrl: `/lightning/r/${r.Id}/view`,
-            ...r
-        }));
-        this.loading = false;
-        this.searchExecuted = true;
+        try {
+            const articles = await getArticles({
+                categoryGroupName: this.selectedCategoryGroupName,
+                categoryName: this.selectedCategoryName
+            });
+            this.articles = articles.map((r) => ({
+                recordUrl: `/lightning/r/${r.Id}/view`,
+                ...r
+            }));
+        } catch (e) {
+            console.error(e);
+            this.errorMessage = this.extractErrorMessage(e);
+        } finally {
+            this.loading = false;
+            this.searchExecuted = true;
+        }
     }
 
-    get noArticles() {
-        return this.articles.length === 0;
-    }
-
-    get numOfArticlesMessage() {
-        return this.label.KCLV_MessageArticleFound.replaceAll('{0}', this.articles.length);
-    }
-
-    get tableContainerHeight() {
-        return this.articles.length === 0 ? 'height:2.125rem;' : `height:${this.articles.length * 1.825 + 2.125}rem `;
-    }
-
+    /**
+     * @private
+     * @param {} column
+     * @param {*} index
+     * @returns
+     */
     formatColumn(column, index) {
         if (index === 0) {
             return {
@@ -152,5 +160,34 @@ export default class KnowledgeCategoryListView extends LightningElement {
             };
         }
         return column;
+    }
+
+    /**
+     * @privates
+     * @param {*} e Error
+     * @returns error message
+     */
+    extractErrorMessage(e) {
+        return typeof e.message === 'string' ? JSON.stringify(e, Object.getOwnPropertyNames(e)) : JSON.stringify(e);
+    }
+
+    get disabledSearchButton() {
+        return !this.selectedCategoryName;
+    }
+
+    get noArticles() {
+        return this.articles.length === 0;
+    }
+
+    get numOfArticlesMessage() {
+        return this.label.KCLV_MessageArticleFound.replaceAll('{0}', this.articles.length);
+    }
+
+    get tableContainerHeight() {
+        return this.articles.length === 0 ? 'height:2.125rem;' : `height:${this.articles.length * 1.825 + 2.125}rem `;
+    }
+
+    get hasError() {
+        return !!this.errorMessage;
     }
 }
